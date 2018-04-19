946949a4440a2c167e79f6c938eb3fdb41cb3b9e
sixsixseven <sixsixseven@users.noreply.github.com>
2018-04-19 18:07:25 +0100

    Snapshot

--- battleship.py
+++ battleship.py
@@ -75,17 +75,20 @@ class Player(object):
 			orientation = random.randint(0,1)	#	0 = horiz, 1 = vert
 			x = random.randint(1,10)
 			y = random.randint(1,10)
-			location_seed = [x, y]
+			location_seed = [[x, y]]
+			print(f"Locaiton seed: {location_seed}")
+			#	IF 'orientation' = 0, then increment on x by the value of Battleship(self.size), if 'orientation' is 1, then increment on y by the value of Battleship(self.size)
+			if orientation == 0:
+				proposed_location = location_seed.append(location_seed[0][0] + 5)#-->Ship size
+			elif orientation == 1:
+				proposed_location = location_seed[0][1] + 5#-->Ship size
+			print(f"Proposed location: {proposed_location}")
+			#	IF any x or y coord in 'proposed_location' are < 1 or > 10, start again, else;
 
-			#	IF 'orientation' is 0, then increment on x by the value of Battleship(self.size), if 'orientation' is 1, then increment on y by the value of Battleship(self.size)
-			proposed_location = [None]
+			#	IF the x AND y coord for any item in 'proposed_location' = any item in self.fleet, start again, else;
 
-			#	IF x or y for any item in 'proposed_location' are < 1 or > 10, start again, else;
-
-			#	IF x or y for any item in 'proposed_location' = any item in self.fleet, start again, else;
-			
 			#	IF there are no collisions, append those values to self.location[] in Battleship().
-			self.fleet.append(Battleship())
+			#self.fleet.append(Battleship())
 			
 
 
